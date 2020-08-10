cff4e5b5f387f43f6f5cb4516ceea183dd291a0a
luis enrique leon hernandez <luis15ago98@gmail.com>
2020-08-10 18:07:22 -0500

    json y preguntas ejemplo

--- inicio.js
+++ inicio.js
@@ -1,1 +1,10 @@
-/* Esperando ver que hacer XD */
+document.addEventListener('DOMContentLoaded',function(){
+
+const pre = document.getElementById('4');
+console.log(pre.parentElement);
+
+setTimeout(function(){
+      pre.parentElement.remove();
+      const nuevo = document.getElementById('pregunta5');
+}, 3000);
+     });
