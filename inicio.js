7571d3c7c375302faca1bb671257388097c363f3
luis enrique leon hernandez <luis15ago98@gmail.com>
2020-08-11 12:08:24 -0500

    mas

--- inicio.js
+++ inicio.js
@@ -1,2 +1,12 @@
-/* sera para validaciones */
-console.log("hola");
+const nom = document.getElementById('usuario');
+const matricula = document.getElementById('matricula');
+const carreras = document.getElementById('carreras');
+const btn = document.getElementById('formdatos');
+btn.addEventListener('submit',function(e){
+     e.preventDefault();
+     const seCarrera = carreras.options[carreras.selectedIndex].value;
+     console.log(seCarrera);
+     
+});
+
+
