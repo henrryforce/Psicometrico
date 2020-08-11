51bdf17564310e25f2b901674be6dcaf88c79399
luis enrique leon hernandez <luis15ago98@gmail.com>
2020-08-10 19:13:21 -0500

    limpie el js de inicio

--- inicio.js
+++ inicio.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded',function(){
-
-const pre = document.getElementById('4');
-console.log(pre.parentElement);
-
-setTimeout(function(){
-      pre.parentElement.remove();
-      const nuevo = document.getElementById('pregunta5');
-}, 3000);
-     });
+/* sera para validaciones */
