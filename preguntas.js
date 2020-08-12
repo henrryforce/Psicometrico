d3ab6765b078437af0d2ce7babba26c85135395c
luis enrique leon hernandez <luis15ago98@gmail.com>
2020-08-11 19:53:14 -0500

    funcion que inyecta las preguntas tipo 1

--- preguntas.js
+++ preguntas.js
@@ -1,7 +1,5 @@
-/*
-programa que ingrese las preguntas y las calidique :D
-huevos wexD*/
-const preg1 = document.getElementById('pregunta1');
+let contpreg=4;
+const preg1 = document.getElementById('preguntas');
 document.addEventListener("DOMContentLoaded", ready);
 function ready() {
      const xhr = new XMLHttpRequest();
@@ -10,7 +8,7 @@ function ready() {
           if (this.status === 200) {
                const preguntas = JSON.parse(this.responseText);
                if (preguntas[0]['tipo'] === 1) {
-                    preg1.firstChild.remove();
+                  /*  
                     preg1.innerHTML = `
                     <article id="${preguntas[0]['id']}">
                     <p>${preguntas[0]['pregunta']}</p>
@@ -26,15 +24,41 @@ function ready() {
                     <option value="4">${preguntas[0]['opciones'][3]}</option>
                     <option value="5">${preguntas[0]['opciones'][4]}</option>
                     </select>
-                    </div>
-                           
+                    </div>            
                         </table>
                     </article> 
-                    `
-                    //console.log(preguntas[0]['opciones'][0])
+                    `;*/
+                    generaPregTip1(preguntas);
 
                }
           }
      }
      xhr.send()
+}
+function generaPregTip1(preguntas){
+   for(var i=0;i<contpreg;i++){
+        
+        var ar = document.createElement('article');
+        ar.setAttribute("id",preguntas[i]['id']);
+        ar.innerHTML=`
+        <p>${preguntas[i]['pregunta']}</p>
+        <table>
+        <div class="input-group mb-3">
+        <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect01">Options</label>
+        </div>
+        <select class="custom-select" id="inputGroupSelect01">
+        <option value="1">${preguntas[i]['opciones'][0]}</option>
+        <option value="2">${preguntas[i]['opciones'][1]}</option>
+        <option value="3">${preguntas[i]['opciones'][2]}</option>
+        <option value="4">${preguntas[i]['opciones'][3]}</option>
+        <option value="5">${preguntas[i]['opciones'][4]}</option>
+        </select>
+        </div>            
+            </table>
+        `;
+
+       preg1.appendChild(ar);
+        
+   }
 }
