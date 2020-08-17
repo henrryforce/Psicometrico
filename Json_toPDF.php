0812cd1be527b857ac81f7c65ccaa07d79970f33
gerardoSV95 <gerardo_s_v@outlook.com>
2020-08-16 19:28:06 -0500

    Nuevo JsontoPdf

--- Json_toPDF.php
+++ Json_toPDF.php
@@ -10,14 +10,32 @@
    # $mpdf -> Ouput();
    
     var_dump($_GET['datos']);
+    ob_start();
+    require('fpdf/fpdf.php');
     
- 
-   
-
-
-#  var_dump($_COOKIE["datosP"]) ;
-#  var_dump($_COOKIE["respuestas"]);
-#  var_dump($_COOKIE["puntaje"]);
-#  var_dump($_COOKIE["tab"]);
+    
+    
+    
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',10);
+    
+    $array = $_GET['datos'];
+    
+    $pdf->Cell(40,10,'Datos');
+    $pdf->Cell(40,10,'Respuestas');
+    $pdf->Ln(10);
+    
+    $array1= $array[0][0];
+    $array2= $array[0][1];
+    
+    foreach($array1 as $key=>$row){
+        $pdf->Cell(40,10,$row);    
+        $pdf->Cell(40,10,$array2[$key]);
+        $pdf->Ln(10);
+    }
+    
+    $pdf->Output();
+    ob_end_flush();
 
 ?>
